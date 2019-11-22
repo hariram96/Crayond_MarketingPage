5c24128712b6fd86c656cc6a33bbd7f16b94e15f
babucrayond <sarath@crayond.co>
2019-11-22 22:41:46 +0530

    update on headers and feedback

--- plugin/loader.js
+++ plugin/loader.js
@@ -3,3 +3,9 @@ window.addEventListener("load",function(){
         $('#pre-loader').fadeOut("500");
     }, 500);
 });
+(function () {
+    const header = document.querySelector('.header_mobile');
+   const icon = document.querySelector('.icon-container');
+   icon.onclick = function () {
+       header.classList.toggle('menu-open');
+   }}());
